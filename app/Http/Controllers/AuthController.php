64afb8eb4302f7c9ddd621712019dc85e4a9ba37
batol-alshab <batolsh2003@gmail.com>
2025-11-06 11:31:36 +0300

    Logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,27 +38,34 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->validate(([
-            'email' =>'required|email|string',
-            'password'=>['required'],
-            'remember'=>'boolean'
+            'email' => 'required|email|string',
+            'password' => ['required'],
+            'remember' => 'boolean'
         ]));
-        $remember = $credentials['remember']?? false;
+        $remember = $credentials['remember'] ?? false;
         unset($credentials['remember']);
         // نحذف remember من المصفوفة $credentials
 
-        if(!Auth::attempt($credentials,$remember))
-        {
+        if (!Auth::attempt($credentials, $remember)) {
             return response([
-                'error' =>'the credentials are not correct'
-            ],422);
+                'error' => 'the credentials are not correct'
+            ], 422);
         }
 
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
         return response([
-            'user'=>$user,
-            'token' =>$token
+            'user' => $user,
+            'token' => $token
         ]);
     }
 
+    public function logout()
+    {
+        $user = Auth::user();
+        $user->currentAccessToken()->delete();
+        return response([
+            'success' => true
+        ]);
+    }
 }
